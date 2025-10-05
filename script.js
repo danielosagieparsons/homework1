1d1d97201aec463357117c6b1c6d688e8c083cea
Daniel Parsons <danielosagieparsons@gmail.com>
2025-10-05 17:03:47 +0100

    Big update to website design and functionality

--- script.js
+++ script.js
@@ -7,15 +7,11 @@ console.log(portfolioButton);
 const homediv = document.getElementById("homediv");
 const home1 = document.getElementById("home1");
 
-
-console.log('Script loaded');
-portfolioButton.addEventListener("click", function() {
-    homediv.setAttribute("hidden", "hidden");
-    home1.setAttribute("hidden", "hidden");
-    console.log('Section is now invisible');
-    portfolio.removeAttribute("hidden");
-    console.log('Portfolio is now visible');
-
+console.log("Script loaded");
+portfolioButton.addEventListener("click", function () {
+  homediv.setAttribute("hidden", "hidden");
+  home1.setAttribute("hidden", "hidden");
+  console.log("Section is now invisible");
+  portfolio.removeAttribute("hidden");
+  console.log("Portfolio is now visible");
 });
-
-
